b2615fab5f3ef0081c7eda69e2a037390b4a35fc
DanzakFromEurope <49127534+DanzakFromEurope@users.noreply.github.com>
2019-03-31 20:18:54 +0200

    Add files via upload

---
+++ js/jQueryFunctions.js
@@ -0,0 +1,6 @@
+//Slideshow
+$(document).ready(function() {
+  $('#showcase').fullClip( {
+      images: ['./imgs/1.jpg', './imgs/2.jpg', './imgs/3.jpg', './imgs/4.jpg', './imgs/5.jpg', './imgs/6.jpg']
+  });
+});
