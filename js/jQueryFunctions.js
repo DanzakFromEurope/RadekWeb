1dc38b11e711efd98ddb5b699de10f72bf8686ec
DanzakFromEurope <49127534+DanzakFromEurope@users.noreply.github.com>
2019-04-04 20:35:15 +0200

    Update jQueryFunctions.js

--- js/jQueryFunctions.js
+++ js/jQueryFunctions.js
@@ -2,6 +2,6 @@
 $(document).ready(function() {
   $('#showcase').fullClip( {
       images: ['./imgs/1.jpg', './imgs/2.jpg', './imgs/3.jpg', './imgs/4.jpg', './imgs/5.jpg', './imgs/6.jpg'],
-      header: ['krokodyl', 'breberka', 'kopyto', 'strom', 'jablko', 'klepeto']
+      header: ["krokodyl", "breberka", "kopyto", "strom", "jablko", "klepeto"]
   });
 });
