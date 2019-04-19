88fd4b44b11550a2487762599d7c408e7de7fa22
DanzakFromEurope <49127534+DanzakFromEurope@users.noreply.github.com>
2019-04-19 17:29:56 +0200

    Delete jQueryFunctions.js

--- js/jQueryFunctions.js
+++
@@ -1,7 +0,0 @@
-//Slideshow
-$(document).ready(function() {
-  $('#showcase').fullClip( {
-      images: ['./imgs/1.jpg', './imgs/2.jpg', './imgs/3.jpg', './imgs/4.jpg', './imgs/5.jpg', './imgs/6.jpg'],
-      header: ["krokodyl", "breberka", "kopyto", "strom", "jablko", "klepeto"]
-  });
-});
